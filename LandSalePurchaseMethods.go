12ce6cf4089076a464c39b6aded1511be9727051
summalho <summalho@in.ibm.com>
2017-05-05 22:42:20 +0530

    Working Code

---
+++ LandSalePurchaseMethods.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"github.com/hyperledger/fabric/core/chaincode/shim"
+)
+
+func transferOwnerShip(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+
+	return nil, nil
+
+}
+
+func listRegisteredProperties(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+
+	return nil, nil
+}
+
+func listRegisteredPropertiesByCity(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+
+	return nil, nil
+}
+
+func listRegisteredPropertiesByCityAndArea(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+
+	return nil, nil
+}
+
+func listRegisteredPropertiesByPincode(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+
+	return nil, nil
+}
+
+func listpropertyHistory(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+
+	return nil, nil
+}
